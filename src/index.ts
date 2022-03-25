8cbeaf7e3194dd3a258ec85c14d316d597b8b6e9
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-03-24 22:34:30 -0500

    Updates plugin name

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import { API } from 'homebridge';
 import { PLATFORM_NAME } from './settings';
-import { EightSleepThermostatPlatformPlugin } from './platform';
-// import { PlatformPluginConstructor } from 'homebridge';
-
-/**
- * This method registers the platform with Homebridge
- */
-// export = (api: API) => {
-//   api.registerPlatform(PLATFORM_NAME,
-//     EightSleepThermostatPlatformPlugin as PlatformPluginConstructor);
-// };
+import { EightSleepThermostatPlatform } from './platform';
 
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME,
-    EightSleepThermostatPlatformPlugin);
+  api.registerPlatform(PLATFORM_NAME, EightSleepThermostatPlatform);
 };
