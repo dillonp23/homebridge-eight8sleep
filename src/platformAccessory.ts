52f5c2393b0a3adefa73b3939d08ef1f6a8800dc
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-04-02 23:52:47 -0500

    Adds a new `TwoWayTempMapper` helper utility
    - Eight Sleep client API doesn't use actual temperatures - uses values between -100 & 100
    -This utility class maps temperatures (displayed in degrees) on the thermostat to relative levels that match the client API
    - Aims to keep temp settings for users consistent, whether temps are changed in Eight Sleep app or from within a HomeKit controller

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -16,6 +16,8 @@ export class EightSleepThermostatAccessory {
     TemperatureDisplayUnits: 1,
   };
 
+  private mapper = new TwoWayTempMapper();
+
   constructor(
     private readonly platform: EightSleepThermostatPlatform,
     private readonly accessory: PlatformAccessory,
@@ -133,6 +135,94 @@ export class EightSleepThermostatAccessory {
       this.Thermostat_data.CurrentHeatingCoolingState);
 
     this.log.debug('Triggered Update of CurrentHeatingCoolingState:', this.Thermostat_data.CurrentHeatingCoolingState);
+  }
+
+
+/**
+ * Maps degrees (°C/°F) to 'level' used by the Eight Sleep client.
+ *
+ * Locally °C ranges from 10°-45°, and °F from 50°-113°, but the client
+ * API uses a value between -100 (max cooling) to +100 (max heating),
+ * independent of the temp units displayed on our thermostat.
+ */
+class TwoWayTempMapper {
+  // Range of cooling levels from client
+  private cooling_level_start = -100;
+  private cooling_level_end = -1;
+
+  /**
+   * Actual min on thermostat is 50, but 8sleep api is weird, see
+   * {@linkcode calculateTempFrom()} method below for more info
+   */
+  // Min & max cooling temps on thermostat:
+  private cooling_temp_start = 61;
+  private cooling_temp_end = 80;
+
+  // Range of heating levels from client
+  private heating_level_start = 1;
+  private heating_level_end = 100;
+
+  // Min & max heating temps on thermostat
+  private heating_temp_start = 81;
+  private heating_temp_end = 113;
+
+  public getTempFrom(level: number) {
+    return this.levels[level];
+  }
+
+  public getLevelFrom(temp: number) {
+    return this.temps[temp];
+  }
+
+  private temps: Record<number, number> = {};
+  private levels: Record<number, number> = {};
+
+  constructor() {
+    this.generateMaps();
+  }
+
+  // Convert client api levels to relative temp for thermostat
+  private generateMaps() {
+    for (let lvl = -100; lvl <= 100; lvl++) {
+      const temp = this.calculateTempFrom(lvl);
+      this.updateRecords(temp, lvl);
+    }
+  }
+
+  private updateRecords(temp: number, level: number) {
+    this.temps[temp] = level;
+    this.levels[level] = temp;
+  }
+
+  private calculateTempFrom(level: number) {
+    switch (true) {
+      case (level <= -88):
+        // Adjust temp according to (seemingly?) arbitrary client api values.
+        // Cooling levels between -100 & -88 from the API correspond to a
+        // single degree difference locally on thermostat
+        return 50 - (-100 - level);
+
+      case (level <= 0):
+        // Cooling levels between -87 & 0
+        return this.getCoolingTemp(level);
+
+      default:
+        // Heating (level > 0)
+        return this.getHeatingTemp(level);
+    }
+  }
+
+  private getCoolingTemp(level: number) {
+    // Gets relative temp for cooling levels between -88 & 0 from client api
+    const slope = (this.cooling_temp_end - this.cooling_temp_start) / (this.cooling_level_end - this.cooling_level_start);
+    const output = this.cooling_temp_start + Math.round(slope * (level - this.cooling_level_start));
+    return output;
+  }
+
+  private getHeatingTemp(level: number) {
+    const slope = (this.heating_temp_end - this.heating_temp_start) / (this.heating_level_end - this.heating_level_start);
+    const output = this.heating_temp_start + Math.round(slope * (level - this.heating_level_start));
+    return output;
   }
 
 }
