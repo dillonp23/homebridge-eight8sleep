5d4dfc8969d82bf8be0395fdf65e84f15a8a6c88
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-04-07 11:48:57 -0500

    Refactors/renames `eightSleepConnection` properties & methods

--- src/eightSleepConnection.ts
+++ src/eightSleepConnection.ts
@@ -1,41 +1,37 @@
 import { readFile, writeFile, mkdir } from 'fs/promises';
 import path from 'path';
 import { EightSleepThermostatPlatform } from './platform';
-import * as Client from './clientRequest';
-import { startIntercepting as axiosMock_startIntercepting } from './axiosMock';
-import {
-  clientAPI,
-  newBedState,
-  bedState,
-  newBedTemp,
-  UserBedSettings,
-  BedState } from './clientRequest';
+import { clientAPI } from './clientRequest';
+import * as AxiosMock from './axiosMock';
 
 const EIGHT_SLEEP_DIR = '8slp';
-const SESSION_CACHE_FILE = 'client-session.txt';
-const GET_ME_CACHE_FILE = 'me.txt';
-type cacheable = string | ClientSessionData | UserDeviceData | UserData;
+const SESSION_CACHE_FILE = '_login.txt';
+const PRIMARY_USER_CACHE_FILE = '_users_me.txt';
+type cacheable = string | object | Session | PrimaryUser;
 
-// Private credentials from Homebridge `config.json`
+// Private credentials loaded from Homebridge `config.json`
 interface UserCredentials {
   email: string;
   password: string;
 }
 
-interface ClientSessionData {
+interface Session {
   expirationDate: string;
   userId: string;
   token: string;
 }
 
-// For now we only care about user's device
-interface UserData {
-  currentDevice: UserDeviceData;
-}
-
-interface UserDeviceData {
-  id: string;
-  side: string; // 'left', 'right', 'solo'
+// Eight Sleep API defines primaryUser as `me`
+// - `currentDevice.id` is a shared property loaded with user `me`
+// - `side` property is specific to primary user `me`, but will be
+//  used to determine how platform accessories are setup:
+//    - `solo` ==> single accessory
+//    - `left` or `right` ==> two accessories
+interface PrimaryUser {
+  currentDevice: {
+    id: string; // shared deviceId - same for both left/right user
+    side: string; // 'left', 'right', 'solo'
+  };
 }
 
 
@@ -43,11 +39,11 @@ export class EightSleepConnection {
   private readonly userCreds: UserCredentials;
   private readonly cacheDir = path.resolve(this.platform.api.user.storagePath(), EIGHT_SLEEP_DIR);
   private readonly sessionCachePath = path.resolve(this.cacheDir, SESSION_CACHE_FILE);
-  private readonly getMeCachePath = path.resolve(this.cacheDir, GET_ME_CACHE_FILE);
+  private readonly primaryUserCachePath = path.resolve(this.cacheDir, PRIMARY_USER_CACHE_FILE);
   public readonly log = this.platform.log;
 
-  public currentSession = this.prepareClientConnection();
-  public currentDevice = this.prepareUserAndDevice();
+  public session = this.prepareSession();
+  public primaryUser = this.preparePrimaryUser();
 
   constructor(
     public readonly platform: EightSleepThermostatPlatform,
@@ -59,27 +55,26 @@ export class EightSleepConnection {
       password: password,
     };
 
-    axiosMock_startIntercepting(clientAPI, this.log);
+    AxiosMock.startIntercepting(clientAPI, this.log);
   }
 
   /**
    * This method will initiate a chain of events to either load the session
    * containing a userId & token info from cache, or send a new login request
-   * to the 8slp Client API to fetch this information.
+   * to the 8slp API to fetch this information.
    *
-   * The result of this method is stored by {@linkcode currentSession} in the
-   * form of `Promise<ClientSessionType | null>`
+   * The result of this method is stored by {@linkcode session} in the
+   * form of `Promise<Session | null>`
    *
    * Associated methods:
-   * {@linkcode establishSession()} // Manages control flow of loading session/logging in
    * {@linkcode loadCachedSession()} // loads cache & verifies token
-   * {@linkcode login()}
+   * {@linkcode login()} // if no cached session, will re-login
    *
    * @returns a Promise containing the loaded/fetched session
    *
-   * @category ClientSessionType
+   * @category Session
    */
-  async prepareClientConnection() {
+  async prepareSession() {
     try {
       let session = await this.loadCachedSession();
       if (!session || !this.isValid(session)) {
@@ -95,11 +90,11 @@ export class EightSleepConnection {
   }
 
   // Catch error here so that a failure doesn't stop login execution from
-  // proceeding when this method returns to `establishSession()`
+  // proceeding when this method returns to `prepareSession()`
   private async loadCachedSession() {
     try {
       const cache = await this.readCache(this.sessionCachePath);
-      return JSON.parse(cache) as ClientSessionData;
+      return JSON.parse(cache) as Session;
     } catch (error) {
       this.log.debug('Error loading session from cache', error);
       return null;
@@ -110,7 +105,7 @@ export class EightSleepConnection {
   // exhausted any chance at a successful session load
   private async login() {
     const response = await clientAPI.post('/login', this.userCreds);
-    const session = JSON.parse(response.data['session']) as ClientSessionData;
+    const session = JSON.parse(response.data['session']) as Session;
     if (!this.isValid(session)) {
       throw new Error('Corrupted session info from ClientAPI');
     }
@@ -118,44 +113,44 @@ export class EightSleepConnection {
     return session;
   }
 
-  private isValid(session: ClientSessionData) {
+  private isValid(session: Session) {
     const tokenExpDate = new Date(session.expirationDate).valueOf();
     return this.verifyFields(session) && tokenExpDate > (Date.now() + 100);
   }
 
-  private verifyFields(session: ClientSessionData) {
+  private verifyFields(session: Session) {
     return (session.token && session.expirationDate && session.userId) ? true : false;
   }
 
-  private updateClientSessionHeaders(session: ClientSessionData) {
+  private updateClientSessionHeaders(session: Session) {
     clientAPI.defaults.headers.common['user-id'] = session.userId;
     clientAPI.defaults.headers.common['session-token'] = session.token;
     // this.log.debug('Updated session headers', JSON.stringify(clientAPI.defaults));
   }
 
   /**
-   * This method will initiate a chain of events to either load the device
-   * containing `id` & `side` properties from cache, or will send a `GET`
-   * request to `/users/me/` of 8slp Client API to fetch the user object
+   * This method will initiate a chain of events to either load the primary user data
+   * containing the device `id` & `side` properties from cache, or will send a `GET`
+   * request to `/users/me/` of 8slp API to fetch the user object
    *
-   * The result of this method is stored by {@linkcode currentDevice} in the
-   * form of `Promise<CurrentDeviceType | null>`
+   * The result of this method is stored by {@linkcode primaryUser} in the
+   * form of `Promise<PrimaryUser | null>`
    *
    * Associated methods:
    * {@linkcode loadCachedUser()}
-   * {@linkcode fetchUserAndDevice()}
+   * {@linkcode fetchPrimaryUser()}
    *
    * @returns a Promise containing the loaded/fetched device
    *
    * @category CurrentDeviceType
    */
-  async prepareUserAndDevice() {
+  async preparePrimaryUser() {
     try {
       const cachedUser = await this.loadCachedUser();
       let device = this.verifyDeviceFor(cachedUser);
       if (!device) {
-        this.eraseCache(this.getMeCachePath);
-        device = await this.fetchUserAndDevice();
+        this.eraseCache(this.primaryUserCachePath);
+        device = await this.fetchPrimaryUser();
       }
       return device;
     } catch (error) {
@@ -166,8 +161,8 @@ export class EightSleepConnection {
 
   private async loadCachedUser() {
     try {
-      const cachedUser = await this.readCache(this.getMeCachePath);
-      return JSON.parse(cachedUser) as UserData;
+      const cachedUser = await this.readCache(this.primaryUserCachePath);
+      return JSON.parse(cachedUser) as PrimaryUser;
     } catch (error) {
       this.log.debug('Error loading user from cache', error);
       return null;
@@ -176,24 +171,24 @@ export class EightSleepConnection {
 
   // GET: Full user profile data from client
   // - use on first time load when no user (i.e. device) previously cached
-  private async fetchUserAndDevice() {
+  private async fetchPrimaryUser() {
     // Must wait for session -> if null, we're unable to fetch user profile
     // as we need the userId & token headers included in the request
-    const session = await this.currentSession;
+    const session = await this.session;
     if (!session) {
       throw new Error('No session');
     }
     const response = await clientAPI.get('/users/me');
-    const user = JSON.parse(response.data['user']) as UserData;
+    const user = JSON.parse(response.data['user']) as PrimaryUser;
     const device = this.verifyDeviceFor(user);
     if (!device) {
       throw new Error('Unable to fetch user profile & current device from client');
     }
-    this.writeToCache(this.getMeCachePath, user);
+    this.writeToCache(this.primaryUserCachePath, user);
     return device;
   }
 
-  private verifyDeviceFor(user: UserData | null) {
+  private verifyDeviceFor(user: PrimaryUser | null) {
     if (!user || !user.currentDevice.id || !user.currentDevice.side) {
       return null;
     }
@@ -202,7 +197,7 @@ export class EightSleepConnection {
 
   /**
    * Caching functionality for both session data (token, userId, tokenExp) and the
-   * user's full profile (we only care about 'currentDevice' from user type)
+   * primary user's full profile (we only care about 'currentDevice')
    *
    * Associated methods:
    * {@linkcode readCache()}
@@ -210,7 +205,7 @@ export class EightSleepConnection {
    * {@linkcode eraseCache()}
    * {@linkcode makeCacheDirectory()}
    *
-   * @category Caching (ClientSessionType & User)
+   * @category Caching {@linkcode Session} & {@linkcode PrimaryUser}
    */
   private async readCache(filepath: string) {
     try {
@@ -234,9 +229,9 @@ export class EightSleepConnection {
 
   private async eraseCache(filepath: string) {
     try {
-      await this.writeToCache(filepath, 'Empty Cache');
+      await this.writeToCache(filepath, {});
     } catch {
-      this.log.debug('Unable to erase user info cache');
+      this.log.debug('Unable to erase cache at:', filepath);
     }
   }
 
