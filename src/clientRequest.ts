df0b778a70069410e03dd595e83edecd17a0f9a2
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-04-07 13:55:51 -0500

    Refactors `clientRequest` module logic
    - Improves generic request methods to allow reusability for fetching and updating client data
    - Removes session data & accessory info from module to separate concerns

--- src/clientRequest.ts
+++ src/clientRequest.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
 import agentkeepalive from 'agentkeepalive';
-import { EightSleepConnection } from './eightSleepConnection';
 
 axios.defaults.headers.common = {
   'Content-Type': 'application/json',
@@ -20,73 +19,46 @@ export interface Request<T> {
   body?: Partial<T>;
 }
 
-const newReqBody = <T extends object>(key: keyof T, data: unknown) => {
+type ClientDataType = string | number | boolean | object;
+
+export const currentState = <T>(endpoint: string) => {
+  return generateRequest<T>(endpoint);
+};
+
+export const newState = <T>(endpoint: string, key: keyof T, newValue: ClientDataType) => {
+  const body = makeReqBody<T>(key, newValue);
+  return generateRequest<T>(endpoint, body);
+};
+
+const makeReqBody = <T>(key: keyof T, data: ClientDataType) => {
   const body: Partial<T> = {};
   body[key as string] = data;
   return body;
 };
 
-const generateRequest = <T>(userId: string, endpoint: string, data?: Partial<T>): Request<T> => {
+const generateRequest = <T>(endpoint: string, data?: Partial<T>): Request<T> => {
   return {
     endpoint: endpoint,
     body: data,
   };
 };
 
-export const bedState = (userId: string) => {
-  return generateRequest(userId, userBedSettingsUrl(userId));
-};
-
-export const newBedState = (userId: string, state: BedState) => {
-  const body = newReqBody<UserBedSettings>('currentState', { type: state });
-  return generateRequest(userId, userBedSettingsUrl(userId), body);
-};
-
-export const newBedTemp = (userId: string, level: number) => {
-  const body = newReqBody<UserBedSettings>('currentLevel', level);
-  return generateRequest(userId, userBedSettingsUrl(userId), body);
-};
-
-export enum BedState {
-  on = 'smart',
-  off = 'off',
-}
-
-interface CurrentState {
-  type: BedState;
-}
-
-const userBedSettingsUrl = (userId: string) => `/users/${userId}/temperature`;
-export interface UserBedSettings {
-  currentLevel: number;
-  currentState: CurrentState;
-}
-
-const clientDeviceSettingsUrl = (deviceId: string) => `/devices/${deviceId}`;
-export interface ClientDeviceSettings {
-  level: number;
-}
-
-export const put = async <T>(connection: EightSleepConnection, req: Request<unknown>) => {
+export const put = async <T>(req: Request<T>) => {
   try {
-    await connection.currentSession;
     const res = await clientAPI.put(req.endpoint, req.body);
     const data = parseClientResponse<T>(res);
     return data;
   } catch (error) {
-    connection.log.error('Unable to PUT device state updates', req.endpoint, error);
     return null;
   }
 };
 
-export const get = async <T>(connection: EightSleepConnection, req: Request<unknown>) => {
+export const get = async <T>(req: Request<T>) => {
   try {
-    await connection.currentSession;
     const res = await clientAPI.get(req.endpoint);
     const data = parseClientResponse<T>(res);
     return data;
   } catch (error) {
-    connection.log.error('Unable to GET device state for request', req.endpoint, error);
     return null;
   }
 };
