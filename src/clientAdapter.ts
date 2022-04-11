303cfdde151c72f8afbc8ba7283fc066b56f5323
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-04-10 19:58:01 -0500

    Adds refresh interval for accessory adapter class
    - Implements logic to automatically refresh accessory state when a controller was recently active (within last 1.5 minutes) before going to standby
    - Updates time intervals for refresh for both platform and accessory adapter

--- src/clientAdapter.ts
+++ src/clientAdapter.ts
@@ -55,7 +55,7 @@ export class PlatformClientAdapter {
   private async loadSharedDeviceState() {
     try {
       const response = await Client.get(currentState<SharedDeviceResponse>(this.devicesEndpoint), this.log);
-      this.log.warn('Fetched device settings:', response);
+      this.log.debug('Fetched device settings');
       return response ? response.result : null;
     } catch (error) {
       this.log.error('Error getting shared device status:', error);
@@ -70,22 +70,22 @@ export class PlatformClientAdapter {
 
   private startRefreshing() {
     // Fetch updated client API temps every 10 seconds (while active)
-    return setInterval(this.refreshState, 1000 * 10);
+    return setInterval(this.refreshState, 1000 * 15);
   }
 
   /**
    * When `GET` CurrentTemp handler fired, update last active timestamp.
-   * Continuing refreshing state every 10 seconds while active, but once
-   * handler hasn't been fired in more than 2 minutes, cancel the refresh
+   * Continuing refreshing state every 15 seconds while active, but once
+   * handler hasn't been fired in more than 1.5 minutes, cancel the refresh
    * interval... i.e. we only continue hitting the client API while a home
    * controller is actively requesting an updated current temperature,
    * otherwise go into standby to prevent unnecessary requests
    */
   private clearRefreshIfNotActive() {
-    this.log.warn('Last active:', new Date(this.lastActive));
-    if (this.refreshInterval && this.lastActive < Date.now() - 1000 * 60 * 2) {
+    this.log.debug('Platform client last active:', new Date(this.lastActive));
+    if (this.refreshInterval && this.lastActive < Date.now() - 1000 * 90) {
       // Go into standby until next time there is controller activity
-      this.log.warn('No activity detected for >2 minutes, entering standby...');
+      this.log.debug('No platform activity detected for >1.5 minute, entering standby...');
       global.clearInterval(this.refreshInterval);
       this.refreshInterval = null;
     }
@@ -95,11 +95,15 @@ export class PlatformClientAdapter {
     this.lastActive = Date.now();
 
     if (!this.refreshInterval) {
-      // Fetch new state & start refreshing every 10 seconds for next 2 minutes
+      // Fetch new state & start refreshing every 15 seconds for next 1.5 minutes
       this.sharedDeviceSettings = this.loadSharedDeviceState();
       this.refreshInterval = this.startRefreshing();
     }
 
+    return this.checkForUpdates(side);
+  }
+
+  async checkForUpdates(side: 'left' | 'right') {
     const currentSettings = await this.sharedDeviceSettings;
     if (side === 'left') {
       return currentSettings ? currentSettings.leftHeatingLevel : 0;
@@ -110,20 +114,27 @@ export class PlatformClientAdapter {
 }
 
 
+
 export class AccessoryClientAdapter {
   private usersEndpoint = resolveUsersUrl(this.accessoryUserId);
   private currentUserSettings = this.fetchCurrentSettings();
 
+  // Time of last `GET` request made by controller to fetch on/off state or target temp
+  private lastActive = Date.now();
+  private refreshInterval?: ReturnType<typeof setInterval> | null;
+
   constructor(
-      readonly accessoryUserId: string,
-      private readonly log: Logger,
-  ) {}
+    readonly accessoryUserId: string,
+    private readonly log: Logger,
+  ) {
+    this.refreshInterval = this.startRefreshing();
+  }
 
   private async fetchCurrentSettings() {
     try {
       // Returns `level` and `currentState`, i.e. mode `type: smart` or `type: off`
       const response = await Client.get(currentState<UserSettings>(this.usersEndpoint), this.log);
-      this.log.debug('Current device state:', response);
+      this.log.debug('Fetched current device state');
       return response;
     } catch (error) {
       this.log.error('Error fetching current user device settings from client');
@@ -131,7 +142,8 @@ export class AccessoryClientAdapter {
     }
   }
 
-  async userTargetLevel() {
+  async getUserTargetLevel() {
+    this.setAccessoryAsActive();
     const settings = await this.currentUserSettings;
     // `currentLevel` represents temp at which bed is *currently set* to
     // i.e. not the measured temp of the bed, but target temp
@@ -139,9 +151,21 @@ export class AccessoryClientAdapter {
   }
 
   // Current Device On/Off Status & Updates
-  async accessoryIsOn() {
+  async getAccessoryIsOn() {
+    this.setAccessoryAsActive();
     const settings = await this.currentUserSettings;
     return settings?.currentState.type !== DeviceMode.off;
+  }
+
+  async checkForUpdates() {
+    const settings = await this.currentUserSettings;
+    if (settings) {
+      const targetLevel = settings.currentLevel;
+      const targetState = settings.currentState.type === DeviceMode.off ? 0 : 3;
+      return [targetState, targetLevel];
+    } else {
+      return [0, 0];
+    }
   }
 
   /**
@@ -181,7 +205,50 @@ export class AccessoryClientAdapter {
 
   // Reset to new value whenever a client `PUT` request returns updated response
   private updateCurrentSettingsFrom(response: UserSettings | null) {
+    this.log.debug('Updated current settings');
+    this.setAccessoryAsActive();
     this.currentUserSettings = Promise.resolve(response);
   }
 
+  private setAccessoryAsActive() {
+    this.lastActive = Date.now();
+
+    if (!this.refreshInterval) {
+      // Fetch new state & start refreshing every 15 seconds for next 1.5 minutes
+      this.currentUserSettings = this.fetchCurrentSettings();
+      this.refreshInterval = this.startRefreshing();
+    }
+  }
+
+  /**
+   * Refresh interval to continue updating state for accessory
+   * while there is Homekit controller actvity
+   */
+  private refreshState = async () => {
+    this.currentUserSettings = this.fetchCurrentSettings();
+    this.clearRefreshIfNotActive();
+  };
+
+  private startRefreshing() {
+    // Fetch updated client accessory state & target temp every 15 seconds (while active)
+    return setInterval(this.refreshState, 1000 * 15);
+  }
+
+  /**
+   * When `GET` CurrentTemp handler fired, update last active timestamp.
+   * Continuing refreshing state every 15 seconds while active, but once
+   * handler hasn't been fired in more than 1.5 minutes, cancel the refresh
+   * interval... i.e. we only continue hitting the client API while a home
+   * controller is actively requesting an updated current temperature,
+   * otherwise go into standby to prevent unnecessary requests
+   */
+  private clearRefreshIfNotActive() {
+    this.log.debug('Accessory client last active:', new Date(this.lastActive));
+    if (this.refreshInterval && this.lastActive < Date.now() - 1000 * 90) {
+      // Go into standby until next time there is controller activity
+      this.log.warn('No accessory activity detected for >1.5 minute, entering standby...');
+      global.clearInterval(this.refreshInterval);
+      this.refreshInterval = null;
+    }
+  }
 }
