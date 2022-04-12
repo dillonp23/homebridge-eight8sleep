d38bafe07420d072c8a4451d14ff98eeede2df6a
Dillon P <52013691+dillonp23@users.noreply.github.com>
2022-04-12 13:45:31 -0500

    Updates plugin name, version and description

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'EightSleepThermostat';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-eight8sleep';
+export const PLUGIN_NAME = 'homebridge-eightsleepthermostat';
